8108256a9530af61efd8b2dbb02f3521cd3f0778
David <mrriptidemc@gmail.com>
2023-08-26 16:22:27 -0400

    files i accidentally ignored

---
+++ app/lib/Providers.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+/* Core */
+import { Provider } from 'react-redux'
+
+/* Instruments */
+import { reduxStore } from '@/lib/redux'
+import { SessionProvider } from 'next-auth/react'
+import { ScheduleProvider } from '../scheduleManager'
+import { ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
+import { ThemeProvider, useTheme } from '@emotion/react'
+import { CachePersistor, LocalStorageWrapper } from 'apollo3-cache-persist'
+import { useState, useEffect } from 'react'
+
+export const Providers = (props: React.PropsWithChildren) => {
+
+    const [client, setClient] = useState<ApolloClient<NormalizedCacheObject>>();
+  
+    useEffect(() => {
+      async function init() {
+        const cache = new InMemoryCache();
+        let newPersistor = new CachePersistor({
+          cache,
+          storage: new LocalStorageWrapper(window.localStorage),
+          debug: true,
+          trigger: 'write',
+        });
+        await newPersistor.restore();
+        setClient(
+          new ApolloClient({
+  
+            uri: 'http://132.145.143.61/graphql',
+            cache,
+          }),
+        );
+      }
+  
+      init().catch(console.error);
+    }, []);
+    
+  const theme = useTheme();
+
+    if (!client) {
+      return <h2>Initializing app...</h2>;
+    }
+  return (
+    <Provider store={reduxStore}>
+        <ScheduleProvider>
+            <SessionProvider>
+                <ApolloProvider client={client}>
+                    <ThemeProvider theme={theme}>
+                        {props.children}
+                    </ThemeProvider>
+                </ApolloProvider>
+            </SessionProvider>
+        </ScheduleProvider>
+    </Provider>
+  )
+}
---
+++ app/lib/WelcomeDialog.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Dialog from '@mui/material/Dialog';
+import { DialogContentText, FormControl, FormGroup, TextField } from '@mui/material';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from 'next/link';
+import { useLocalStorage } from '../localstorage';
+
+
+export default function WelcomeDialog() {
+    const [open, setOpen] = React.useState(true);
+    const [state, setState] = React.useState({
+        ads: true,
+        analytics: true,
+        terms: false,
+    });
+
+    const { ads, analytics, terms } = state;
+    const [termsError, setTermsError] = React.useState(true);
+
+    const handleClose = (event: object, reason: string) => {
+        if (reason && reason == "backdropClick") 
+            return;
+      setOpen(false);
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({
+          ...state,
+          [event.target.name]: event.target.checked,
+        });
+      };
+
+    const onAccept = () => {
+        console.log(state)
+        if (terms) {
+            localStorage.setItem("ads", JSON.stringify(ads));
+            localStorage.setItem("analytics", JSON.stringify(analytics));
+            localStorage.setItem("terms", JSON.stringify(true));
+            setOpen(false);
+        } else {
+            setTermsError(true);
+        }
+    }
+
+    return (
+        <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Welcome to Tarheel Compass!</DialogTitle>
+        <DialogContent>
+            <DialogContentText>
+                We're happy to have you, but before you can continue, we have to ask a few legal questions.
+            </DialogContentText>
+            <DialogContentText fontWeight="bold">
+                <br/>
+                Cookie usage
+            </DialogContentText>
+            <FormGroup>
+                <FormControlLabel control={<Checkbox checked={ads} onChange={handleChange} name="ads" />} label="Ad Personalization" />
+                <FormControlLabel control={<Checkbox checked={analytics} onChange={handleChange} name="analytics" />} label="Analytics" />
+                <FormControlLabel disabled control={<Checkbox defaultChecked />} label="Essential" />
+            </FormGroup>
+            <DialogContentText>
+                These settings can be changed at any time from the settings page.
+            </DialogContentText>
+            <DialogContentText fontWeight="bold">
+                <br/>
+                Terms of Service and Privacy Policy
+            </DialogContentText>
+            <FormControl error={termsError}>
+                <FormControlLabel control={<Checkbox checked={terms} onChange={handleChange} name="terms" />} label={
+                    <div>
+                        <span>I have read and accept the </span>
+                        <Link href="/terms">terms of use</Link>
+                        <span> and </span>
+                        <Link href="/privacy">privacy policy</Link>
+                    </div>
+                    } />
+            </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onAccept}>Accept</Button>
+        </DialogActions>
+      </Dialog>
+    );
+}
