0b56b0439f54e4af188bd5c126ff22ee606e7f67
David <mrriptidemc@gmail.com>
2024-02-27 15:06:44 -0500

    create a favicon
    may change the design later, not sure if theres enough contrast/size for it to be visible

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,16 @@ export default function Layout({
   return (
     <html lang="en">
       <head>
-        <title>Tarheel Compass</title>
+        <title>Tarheel Commpass</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4b9cd3" />
+        <meta name="apple-mobile-web-app-title" content="Tarheel Compass" />
+        <meta name="application-name" content="Tarheel Compass" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
         <Providers>
