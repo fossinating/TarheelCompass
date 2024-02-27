03f38283171534f79bccecac058e3ce33d3cafa7
David <mrriptidemc@gmail.com>
2024-02-27 15:11:17 -0500

    oops i brought back a spelling mistake on accident

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({
   return (
     <html lang="en">
       <head>
-        <title>Tarheel Commpass</title>
+        <title>Tarheel Compass</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
