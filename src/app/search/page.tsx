6bd1c97379873dc7fe05ad0d7c3749ca27c66248
David Foss <david@fosshome.com>
2024-07-05 00:13:28 -0500

    fix terms

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,7 +5,7 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { useRef, useState } from "react";
 import { gql } from "../../__generated__";
 import ClassDisplay from "../lib/ClassDisplay";
-import { SectionData, useTerms } from "../lib/Common";
+import { SectionData, titleCase, useTerms } from "../lib/Common";
 import "./Search.css";
 
 export interface SnackbarMessage {
@@ -52,6 +52,19 @@ export default function Page() {
     setTerm(event.target.value as string);
   };
 
+  const convertTermName = (name: string|undefined) => {
+    if (name == undefined) { 
+      return "Undefined Term";
+    }
+
+    const first_underscore = name.indexOf("_");
+    const first_bit = name.substring(0, first_underscore);
+    const last_bit = name.substring(first_underscore);
+
+
+    return titleCase(first_bit) + last_bit.replaceAll("_", " ");
+  }
+
   let class_numbers: Array<number> = [];
 
   return (
@@ -77,7 +90,7 @@ export default function Page() {
               >
                 {terms != undefined && terms.length > 0 ? 
                   terms.map((term) =>
-                    <MenuItem key={term.id} value={term.id}>{term.name}</MenuItem>
+                    <MenuItem key={term.id} value={term.id}>{convertTermName(term.name)}</MenuItem>
                   )
                   : <MenuItem value="Loading">Loading</MenuItem>
                 }
